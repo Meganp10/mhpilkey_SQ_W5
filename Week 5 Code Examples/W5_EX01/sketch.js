0eb265d884e8b1445ec8c5f65630cfe33ddb439e
Meganp10 <mhpilkey@uwaterloo.ca>
2026-06-10 10:46:28 -0400

    name change

--- Week 5 Code Examples/W5_EX01/sketch.js
+++ Week 5 Code Examples/W5_EX01/sketch.js
@@ -69,7 +69,7 @@ let characterSheet; // the loaded sprite sheet image
 // ============================================================
 function preload() {
   // loadImage() loads the sprite sheet before setup() runs
-  characterSheet = loadImage("assets/images/walking.png");
+  characterSheet = loadImage("Frog boy sprite sheet.jpeg");
 }
 
 // ============================================================
